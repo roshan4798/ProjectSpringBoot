f1183aa2651363c486d3819995d00a9312e66963
unknown <manishjha4798@gmail.com>
2021-08-13 11:41:38 +0530

    first commit

---
+++ src/test/java/com/vwit/demoaws/DemoawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vwit.demoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
